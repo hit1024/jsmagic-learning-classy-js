97bb7b7152a46908f728c0f940f4b54f4d7677b7
tianyu <imty42@gmail.com>
2016-10-12 23:02:58 +0800

    i know ..

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 function Class (cls) {
-    function func(a, b) {
-        //var args = arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments);
-        //para = arguments;
-        //console.log(arguments);
-        var initialize = cls.initialize || function(a, b){};
-        initialize.call(this, a, b);
-        //cls.initialize.call(func, a, b);
-    }
+    var initialize = cls.initialize || function() {};
+    func = initialize;
     func.prototype.getA = function() { return this.a;}
     func.prototype.getB = function() { return this.b;}
     //console.log(func)
