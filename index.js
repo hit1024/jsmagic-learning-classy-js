8c9a1d4a9314cbdace54d61ae25bc0f4aec41bca
tianyu <imty42@gmail.com>
2016-10-19 12:04:12 +0800

    add func: super

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-function Class (cls) {
-    var initialize = cls.initialize || function() {};
-    func = initialize;
-    func.prototype.getA = function() { return this.a;}
-    func.prototype.getB = function() { return this.b;}
-    //console.log(func)
-    //console.log(func.toString())
+function Class (cls, parent = Object) {
+    func = cls.initialize || function() {};
+    pnt = parent.initialize || function() {};
+
+    var hasProp = {}.hasOwnProperty;
+
+
+    for (var key in parent.prototype) {
+        //console.log('parent key', key);
+        if (hasProp.call(parent.prototype, key) && key != 'initialize'){
+            pnt.prototype[key] = parent.prototype[key];
+        }
+    }
+        
+    func.prototype.__proto__ = parent.prototype;
+
+    for (var key in cls) {
+        //console.log('cls key', key);
+        if (hasProp.call(cls, key) && key != 'initialize'){
+            func.prototype[key] = cls[key];
+        }
+    }
+    func.__super__ = parent;
+    console.log('parent prototype ', parent.prototype);
+
+    func.prototype.super = function (methodName, arg1, arg2) {
+        //return x.call(func.__super__, arg1, arg2);
+        if (methodName == 'self') {
+            return this;
+        }
+        pp = parent.prototype;
+        console.log('parent.name ', pp.name);
+        console.log('__proto__.name ', this.__proto__.name);
+        //console.log('methodName ', methodName);
+        console.log('parent.methodName ', this.__proto__[methodName]);
+        //console.log('parent.foo ', pp.foo);
+        return pp[methodName].call(pp, arg1, arg2);
+        //return this.__proto__[methodName](arg1, arg2);
+        //return pp[methodName].call(this.__proto__, arg1, arg2);
+    }
 
     return func;
 }
